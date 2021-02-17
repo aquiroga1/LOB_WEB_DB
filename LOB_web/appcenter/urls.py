1d7fdbdf5c416bfc7f3e9f36c0b6e78caf9f8edb
Vinicius <vinicius.costa120@gmail.com>
2021-02-17 14:33:43 -0300

    Changes

--- LOB_web/appcenter/urls.py
+++ LOB_web/appcenter/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.center, name="Central de Aplicativos"),
+    path('',views.center, name="Central de Apps"),
     
 ]
 
