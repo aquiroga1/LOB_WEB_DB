9d9d5dd64f5a2798518f14b62cc4e12d436a0964
Adriana <adrdantasrodrigues@gmail.com>
2021-02-17 15:23:50 -0300

    login form

--- LOB_web/loginapp/views.py
+++ LOB_web/loginapp/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, get_user_model
+from .forms import LoginForm
+
+
+def home_page(request):
+    context = {
+                    "title": "Home Page",
+                    "content": "Welcome to Home Page",
+              }
+
+    # Conteúdo da home_page para usuários válidos.
+    if request.user.is_authenticated:
+        context["log_in"] = "User logged in"
+    return render(request, "home.html", context)
+
+
+def login_page(request):
+    # Puxando form.
+    form = LoginForm(request.POST or None)
+    context = {
+                    "form": form
+              }
+
+    #print("User logged in")
+    #print(request.user.is_authenticated)
+
+    # Veficando se os dados são válidos, primeiro authenticate e depois login.
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        # print(user)
+        # print(request.user.is_authenticated)
+
+        if user is not None:
+            login(request, user)
+            print(request.user.is_authenticated)
+            print("Valid login")
+            # Redireciona para uma página de sucesso.
+            return redirect("/")
+        else:
+            # Retorna uma mensagem de erro de 'invalid login'.
+            print("Invalid login")
+
+    return render(request, "auth/login.html", context)
+
+# O get_user_model serve para retornar o model de user ativo no momento.
+User = get_user_model()
--- LOB_web/main_web/settings.py
+++ LOB_web/main_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loginapp',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'main_web.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main_web/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
