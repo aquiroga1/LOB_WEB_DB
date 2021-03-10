ebaa2ccdabb0d9c2972eef9f79e54742c896f622
Adriana <adrdantasrodrigues@gmail.com>
2021-03-10 15:38:16 -0300

    redirect appcenter to login page

--- LOB_web/appcenter/views.py
+++ LOB_web/appcenter/views.py
@@ -1,11 +1,14 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from appcenter.models import App
 
 # Create your views here.
 
 def appcenter(request):
-    
-    apps = App.objects.all()
-    return render(request, "appcenter/appcenter.html", {"apps": apps})
+    if request.user.is_authenticated:
+        apps = App.objects.all()
+        return render(request, "appcenter/appcenter.html", {"apps": apps})
+    else:
+        return redirect("/login")
+
 
