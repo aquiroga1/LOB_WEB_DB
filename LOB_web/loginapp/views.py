478281c99753d15840e3d4276cc42c2513d8dcdc
Adriana <adrdantasrodrigues@gmail.com>
2021-03-19 23:58:38 -0300

    if youre logged in the login page link redirects to appcenter

--- LOB_web/loginapp/views.py
+++ LOB_web/loginapp/views.py
@@ -17,40 +17,42 @@ def home_page(request):
         return redirect("/login")
 
 
-
 def login_page(request):
-    # Puxando form.
-    form = LoginForm(request.POST or None)
-    context = {
-                    "form": form
-              }
+    if request.user.is_authenticated:
+        return render(request, "appcenter/appcenter.html")
+    else:
+        # Puxando form.
+        form = LoginForm(request.POST or None)
+        context = {
+                        "form": form
+                  }
 
-    #print("User logged in")
-    #print(request.user.is_authenticated)
+        #print("User logged in")
+        #print(request.user.is_authenticated)
 
-    # Veficando se os dados são válidos, primeiro authenticate e depois login.
-    if form.is_valid():
-        print(form.cleaned_data)
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
+        # Veficando se os dados são válidos, primeiro authenticate e depois login.
+        if form.is_valid():
+            print(form.cleaned_data)
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
 
-        # print(user)
-        # print(request.user.is_authenticated)
+            # print(user)
+            # print(request.user.is_authenticated)
 
-        if user is not None:
-            login(request, user)
-            print(request.user.is_authenticated)
-            print("Valid login")
-            # Redireciona para uma página de sucesso.
-            return redirect("/appcenter")
-        else:
-            # Retorna uma mensagem de erro de 'invalid login'.
-            print("Invalid login")
-            messages.error(request,'Incorrect username or password.')
-            return redirect("/login")
+            if user is not None:
+                login(request, user)
+                print(request.user.is_authenticated)
+                print("Valid login")
+                # Redireciona para uma página de sucesso.
+                return redirect("/appcenter")
+            else:
+                # Retorna uma mensagem de erro de 'invalid login'.
+                print("Invalid login")
+                messages.error(request,'Incorrect username or password.')
+                return redirect("/login")
 
-    return render(request, "login_app/login.html", context)
+        return render(request, "login_app/login.html", context)
 
 def logout_page(request):
     # if request.method == "POST":
