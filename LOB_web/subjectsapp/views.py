db6b81619b0d6d560a53b77c0b8999f7dfa0cbda
Adriana <adrdantasrodrigues@gmail.com>
2021-03-19 23:31:20 -0300

    subjects page to login if not logged in

--- LOB_web/subjectsapp/views.py
+++ LOB_web/subjectsapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 
 
 def subjects_page(request):
-    form = SubjectsForm()
-    return render(request, "subjects_app/subjects.html", {'form': form})
+    if request.user.is_authenticated:
+        form = SubjectsForm()
+        return render(request, "subjects_app/subjects.html", {'form': form})
+    else:
+        return redirect("/login")
+
 
