b7a34c07f94115b2900b74a44bf7b7a077d4ac11
Adriana <adrdantasrodrigues@gmail.com>
2021-03-26 13:04:13 -0300

    main working

--- LOB_web/appcenter/views.py
+++ LOB_web/appcenter/views.py
@@ -2,7 +2,6 @@ from django.http import request
 from django.shortcuts import render, redirect
 from appcenter.models import App
 
-# Create your views here.
 
 def appcenter(request):
     if request.user.is_authenticated:
