d16a2f2c5ab0dba9514b24ed2ae805bc2c303fa6
Vinicius <vinicius.costa120@gmail.com>
2021-04-09 19:27:45 -0300

    Put all apps in admin and include Projects in LOB CENTER

--- LOB_web/measurementsapp/admin.py
+++ LOB_web/measurementsapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class MeasurementAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+admin.site.register(Measurements, MeasurementAdmin)
