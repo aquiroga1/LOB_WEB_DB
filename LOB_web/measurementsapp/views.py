232388bf6f4944c5722fbe2dc40b16d788fb04db
Vinicius <vinicius.costa120@gmail.com>
2021-09-20 19:44:38 -0300

    Commit order.by

--- LOB_web/measurementsapp/views.py
+++ LOB_web/measurementsapp/views.py
@@ -45,11 +45,11 @@ def measurement_datatable(request):
 
 
 def download(request, path):
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
-    print(file_path)
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
+    
+    print(path)
+    if os.path.exists(path):
+        with open(path, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type='application/file_name')
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            response['Content-Disposition'] = path
             return response
     raise Http404
